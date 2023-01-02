df0195e977a2d4d416c48640de849cb4c067d28a
Andy Marks <vampwillow@gmail.com>
2023-01-03 09:46:40 +1100

    Trying to align SVG rendering with usage of ChunkyPNG from book

--- src/main/kotlin/ImageMazePainter.kt
+++ src/main/kotlin/ImageMazePainter.kt
@@ -5,6 +5,8 @@ import com.github.nwillc.ksvg.elements.SVG
 import java.io.FileWriter
 import kotlin.math.roundToInt
 
+private const val CELL_SIZE = 10
+
 class ImageMazePainter(private val grid: Grid) {
 
     fun paint(fileName: String = "maze.svg") {
@@ -13,8 +15,8 @@ class ImageMazePainter(private val grid: Grid) {
 
     private fun renderGrid(fileName: String) {
         val svg = SVG.svg(true) {
-            height = (grid.rows * 10).toString()
-            width = (grid.columns * 10).toString()
+            height = (grid.rows * CELL_SIZE).toString()
+            width = (grid.columns * CELL_SIZE).toString()
             style {
                 body = """
 
@@ -30,14 +32,17 @@ class ImageMazePainter(private val grid: Grid) {
     }
 
     private fun Container.cell(cell: Cell) {
-        val cellX = cell.column * 10
-        val cellY = cell.row * 10
+        val cellX = cell.column * CELL_SIZE
+        val cellY = cell.row * CELL_SIZE
+        val nextCellX = (cell.column + 1) * CELL_SIZE
+        val nextCellY = (cell.row + 1) * CELL_SIZE
+
         g {
             id = "cell"
             rect {
                 cssClass = "white-stroke"
-                width = "11"
-                height = "11"
+                width = "${nextCellX - cellX}"
+                height = "${nextCellY - cellY}"
                 x = "$cellX"
                 y = "$cellY"
                 fill = "white"
@@ -45,8 +50,8 @@ class ImageMazePainter(private val grid: Grid) {
             if (cell !is NullCell) {
                 rect {
                     cssClass = "black-stroke"
-                    width = "11"
-                    height = "11"
+                    width = "${nextCellX - cellX}"
+                    height = "${nextCellY - cellY}"
                     x = "$cellX"
                     y = "$cellY"
                     fill = styleFor(cell)
@@ -56,36 +61,24 @@ class ImageMazePainter(private val grid: Grid) {
                 id = "doors"
                 cssClass = "black-stroke"
                 if (!cell.isLinkedTo(cell.east)) {
-                    eastWall(cellX, cellY)
+                    verticalWall(nextCellX, cellY)
                 }
                 if (!cell.isLinkedTo(cell.west)) {
-                    westWall(cellX, cellY)
+                    verticalWall(cellX, cellY)
                 }
                 if (!cell.isLinkedTo(cell.north)) {
-                    northWall(cellX, cellY)
+                    horizontalWall(cellX, cellY)
                 }
                 if (!cell.isLinkedTo(cell.south)) {
-                    southWall(cellX, cellY)
+                    horizontalWall(cellX, nextCellY)
                 }
             }
         }
     }
 
-    private fun G.southWall(cellX: Int, cellY: Int) {
+    private fun G.horizontalWall(cellX: Int, cellY: Int) {
         rect {
-            id = "south-wall"
-            width = "10"
-            height = "1"
-            x = "$cellX"
-            y = "${cellY + 10}"
-            fill = "black"
-        }
-    }
-
-    private fun G.northWall(cellX: Int, cellY: Int) {
-        rect {
-            id = "north-wall"
-            width = "10"
+            width = "11"
             height = "1"
             x = "$cellX"
             y = "$cellY"
@@ -93,23 +86,11 @@ class ImageMazePainter(private val grid: Grid) {
         }
     }
 
-    private fun G.westWall(cellX: Int, cellY: Int) {
+    private fun G.verticalWall(cellX: Int, cellY: Int) {
         rect {
-            id = "west-wall"
             width = "1"
-            height = "10"
+            height = "11"
             x = "$cellX"
-            y = "$cellY"
-            fill = "black"
-        }
-    }
-
-    private fun G.eastWall(cellX: Int, cellY: Int) {
-        rect {
-            id = "east-wall"
-            width = "1"
-            height = "10"
-            x = "${cellX + 10}"
             y = "$cellY"
             fill = "black"
         }
